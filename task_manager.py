d93fe0af0b2a0742ecfcae19232e79cf4c75ee31
Kanta <p.kantath@gmail.com>
2025-04-30 01:24:59 +0200

    Add function add_task

--- task_manager.py
+++ task_manager.py
@@ -12,3 +12,17 @@ def load_tasks():
 def save_tasks(data):
     with open(FILENAME, 'w') as f:
         json.dump(data, f, indent=4)
+
+def add_task():
+    tasks = load_tasks()
+    task = {
+        "id": 1,
+        "name": "งานตัวอย่าง",
+        "description": "ยังไม่รับค่าจากผู้ใช้",
+        "due_date": "2025-05-01",
+        "completed": False
+    }
+    
+    tasks.append(task)
+    save_tasks(tasks)
+    print("✅ เพิ่มงานสำเร็จ (ทดสอบ)")
